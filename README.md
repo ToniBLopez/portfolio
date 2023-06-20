e0eaf2a7417cd39d6e2e2e6fc762a385dc24e345
ToniBLopez <toniblopezdeveloper@gmail.com>
2023-06-20 14:43:47 +0200

    I create the About and Experience sections

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Welcome to my portfolio
 This is my third and current 3D portfolio
+
+## Description
+
+
+### Tools
+- [Three.js](https://threejs.org/) a powerful 3D graphics library
+- [React-three-fiber](https://docs.pmnd.rs/react-three-fiber/getting-started/introduction) a react render for three.js
+- [Tailwindcss](https://tailwindcss.com/) a popular utility-first CSS styling framework
+- [Framer Motion](https://www.framer.com/motion/) the most popular library used to bring your React website to life with animations
+
+  #### Others
+  - [Vite](https://vitejs.dev/) providing a fast and efficient development experience
+  - [ESLint](https://eslint.org/) used to identify and report patterns or potential issues in JavaScript code
+  - [Logo.com](https://logo.com/) simple and fast way to build a brand logo
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
-const Computers = () => {
+import CanvasLoader from '../Loader'
+
+const Computers = ({ isMobile, isIPad }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight
+        intensity={0.15}
+        groundColor='black'
+      />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isIPad ? 0.55 : isMobile ? 0.35 : 0.75}
+        position={isIPad ? [0, -2.5, -1] : isMobile ? [0, -1.5, -0.75] : [0, -3.5, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const computerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+  const [isIPad, setisIPad] = useState(false)
+
+  useEffect(() => {
+    const mediaQueryMobile = window.matchMedia('(max-width: 550px)')
+    setIsMobile(mediaQueryMobile.matches)
+    const handleMediaQueryMobileChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    mediaQueryMobile.addEventListener('change', handleMediaQueryMobileChange)
+
+    const mediaQueryIPad = window.matchMedia('(min-width: 550px) and (max-width: 800px)')
+    setisIPad(mediaQueryIPad.matches)
+    const handleMediaQueryIPadChange = (event) => {
+      setisIPad(event.matches)
+    }
+    mediaQueryIPad.addEventListener('change', handleMediaQueryIPadChange)
+
+    return () => {
+      mediaQueryMobile.removeEventListener('change', handleMediaQueryMobileChange)
+      mediaQueryIPad.removeEventListener('change', handleMediaQueryIPadChange)
+    }
+  }, [])
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />} >
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} isIPad={isIPad} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default computerCanvas
