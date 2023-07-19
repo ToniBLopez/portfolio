9baf32aa8468ac9fb4c695baf037fc7ee31fb2e6
ToniBLopez <toniblopezdeveloper@gmail.com>
2023-07-19 19:42:28 +0200

    v0.1.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode> //  Is only for development mode, remove for production
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Navigate to='/portfolio' />} />
+      <Route path='/portfolio'>
+        <Route index element={<App />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   // </React.StrictMode>,
 )
