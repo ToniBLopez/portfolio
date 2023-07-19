5778abfd1ad5a787e9504d474cca5b860553f814
ToniBLopez <toniblopezdeveloper@gmail.com>
2023-07-19 19:47:06 +0200

    v0.1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom'
 import {
   About,
   Contact,
@@ -12,21 +13,23 @@ import {
 
 const App = () => {
   return (
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center' >
-        <Navbar />
-        <Hero />
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center' >
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className='relative z-0'>
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode> //  Is only for development mode, remove for production
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Navigate to='/portfolio' />} />
-      <Route path='/portfolio'>
-        <Route index element={<App />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <App />
   // </React.StrictMode>,
 )
